7ccdaef8edffa1f7f60819b380964bb7b3fa2dd4
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-12-07 08:57:25 +0330

    allert dialog added ==> if can't connect (connection failed) new views id idded==> to can gone during the connection

--- app/src/main/java/com/example/covid_19/fragments/HomeFragment.java
+++ app/src/main/java/com/example/covid_19/fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.covid_19.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,9 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
@@ -41,9 +45,11 @@ public class HomeFragment extends Fragment {
     List<Country> countries = new ArrayList<>();// list of the info country
     View view;
     TextView TotalConfirmed, TotalDeaths, TotalRecovered, connect;
-    CounterView NewConfirmed , NewDeaths;
+    TextView txt_24_hours, txt_Total_country;
     RequestQueue requestQueue;
-    LinearLayout parent;
+    CounterView NewConfirmed, NewDeaths;
+    RelativeLayout relative_main, relative_top;
+    CardView death_cardview, infected_cardView, Total_death_card, total_recovered_card, total_infected_card;
     SpinKitView spin_kit;
 
 
@@ -67,13 +73,21 @@ public class HomeFragment extends Fragment {
 
     // make the component as new
     private void initViews() {
+        txt_24_hours = view.findViewById(R.id.txt_24_hours);
+        txt_Total_country = view.findViewById(R.id.txt_Total_country);
+        death_cardview = view.findViewById(R.id.death_cardview);
+        infected_cardView = view.findViewById(R.id.infected_cardView);
+        Total_death_card = view.findViewById(R.id.Total_death_card);
+        total_recovered_card = view.findViewById(R.id.total_recovered_card);
+        total_infected_card = view.findViewById(R.id.total_infected_card);
         NewConfirmed = view.findViewById(R.id.NewConfirmed);
         TotalConfirmed = view.findViewById(R.id.TotalConfirmed);
         NewDeaths = view.findViewById(R.id.NewDeaths);
         TotalDeaths = view.findViewById(R.id.TotalDeaths);
         TotalRecovered = view.findViewById(R.id.TotalRecovered);
-        parent = view.findViewById(R.id.parent);
+        relative_top = view.findViewById(R.id.relative_top);
         spin_kit = view.findViewById(R.id.spin_kit);
+        relative_main = view.findViewById(R.id.relative_main);
         connect = view.findViewById(R.id.connect);
     }
 
@@ -105,7 +119,8 @@ public class HomeFragment extends Fragment {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.d("Error", "connection failed" + " ");
-                Toast.makeText(getContext(), "اتصال اینترنت را چک کنید", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "check the internet connection...", Toast.LENGTH_SHORT).show();
+                alertDialog();
             }
         });
 
@@ -116,7 +131,7 @@ public class HomeFragment extends Fragment {
     //set info in component
     private void setInfo() {
         DecimalFormat decimalFormat = new DecimalFormat("###,###");// set the decimal format
-        
+
         TotalConfirmed.setText(decimalFormat.format(Integer.valueOf(countries.get(0).getCases())));
         TotalDeaths.setText(decimalFormat.format(Integer.valueOf(countries.get(0).getDeaths())));
         TotalRecovered.setText(decimalFormat.format(Integer.valueOf(countries.get(0).getRecovered())));
@@ -129,7 +144,34 @@ public class HomeFragment extends Fragment {
         // make the spinKit gone and other visible
         spin_kit.setVisibility(View.GONE);
         connect.setVisibility(View.GONE);
-        parent.setVisibility(View.VISIBLE);
+        total_infected_card.setVisibility(View.VISIBLE);
+        txt_24_hours.setVisibility(View.VISIBLE);
+        txt_Total_country.setVisibility(View.VISIBLE);
+        total_recovered_card.setVisibility(View.VISIBLE);
+        Total_death_card.setVisibility(View.VISIBLE);
+        infected_cardView.setVisibility(View.VISIBLE);
+        death_cardview.setVisibility(View.VISIBLE);
+        relative_top.setVisibility(View.VISIBLE);
+        relative_main.setVisibility(View.VISIBLE);
+
+    }
+
+    // when error in connection
+    private void alertDialog() {
+        try {
+            AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage("Alert message to be shown");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "reconnect",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            RequestAPI();
+                        }
+                    });
+            alertDialog.show();
+        } catch (Exception e) {
+            e.getMessage();
+        }
 
     }
 
