11bb11f08af0dfbac50390b2aca6d5d090b8338f
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-03-12 23:46:39 +0330

    app info ==> refrences and developer info

---
+++ app/src/main/java/com/mohammadKZ/CoronaVirus_COVID19_statistics/fragments/AboutFragment.java
@@ -0,0 +1,30 @@
+package com.mohammadKZ.CoronaVirus_COVID19_statistics.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.covid_19.R;
+
+
+public class AboutFragment extends Fragment {
+
+    View view;
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_about, container, false);
+        return view;
+    }
+}
